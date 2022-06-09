3fa9a0527a3ec4847d5027197afb8814151480ee
Jonathan Chan <jonoccchan@gmail.com>
2022-06-09 11:47:07 +0100

    send message #1

--- app.py
+++ app.py
@@ -8,6 +8,18 @@ app.config["DEBUG"] = True
 
 @app.route('/')
 def index():
+    CONNECTION_STR = "Endpoint=sb://jcbugsystem.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=RnKgBHavWXnydS3wUygMQaBT/b7/bXDTWFEjtd5P2zY="
+    PRIO_QUEUE_NAME = "priorityqueue"
+    msg = json.dumps({
+	"severity": "black",
+	"triage": "asdasdasda aSF ASFG ADEG  gqweg gq1 13 41 fa n 133r ",
+	"team" : "pearl",
+        "title" : "test"
+    })
+    with ServiceBusClient.from_connection_string(CONNECTION_STR) as client:
+        with client.get_queue_sender(queue_name=PRIO_QUEUE_NAME) as sender:
+            message = ServiceBusMessage(msg)
+            sender.send_messages(message)
     return render_template('index.html')
 
 if __name__ == '__main__':
