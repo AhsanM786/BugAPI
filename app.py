08cb84b33fa84d7cc89ec8fb5c5888bd698ccab8
Jonathan Chan <jonoccchan@gmail.com>
2022-06-09 11:18:45 +0100

    trial detect route

--- app.py
+++ app.py
@@ -13,13 +13,6 @@ def index():
 
 @app.route("/detect", methods=['POST'])
 def detect():
-    msg = json.dumps({
-	"severity": "black",
-	"triage": "asdasdasda aSF ASFG ADEG  gqweg gq1 13 41 fa n 133r ",
-	"team" : "pearl",
-    "title" : "test"
-    })
-    p_queue(msg)
     return 'OK
 
 
