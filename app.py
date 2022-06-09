ef506cc0259f525ede189b2f8f9a8691fac591ed
Jonathan Chan <jonoccchan@gmail.com>
2022-06-09 08:22:51 +0100

    removed variables

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
 import flask
+import os
 import json
 from flask import Flask, request, jsonify, render_template, redirect, url_for, send_from_directory
 from azure.servicebus import ServiceBusClient, ServiceBusMessage
 
-CONNECTION_STR = "Endpoint=sb://jcbugsystem.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=RnKgBHavWXnydS3wUygMQaBT/b7/bXDTWFEjtd5P2zY="
-PRIO_QUEUE_NAME = "priorityqueue"
-STANDARD_QUEUE_NAME = "standardqueue"
-servicebus_client = ServiceBusClient.from_connection_string(conn_str=CONNECTION_STR, logging_enable=True)
+
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
