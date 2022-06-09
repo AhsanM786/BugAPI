cade6564eb8e24317d634f273bbb5bbf31dc5346
Jonathan Chan <jonoccchan@gmail.com>
2022-06-09 11:15:12 +0100

    trial detect route

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import flask
 import json
 from flask import Flask, request, jsonify, render_template, redirect, url_for, send_from_directory
 from azure.servicebus import ServiceBusClient, ServiceBusMessage
+from servicebus import p_queue
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
@@ -10,6 +11,17 @@ app.config["DEBUG"] = True
 def index():
     return render_template('index.html')
 
-@app.route('')
+@app.route("/detect", methods=['POST'])
+def detect():
+    msg = json.dumps({
+	"severity": "black",
+	"triage": "asdasdasda aSF ASFG ADEG  gqweg gq1 13 41 fa n 133r ",
+	"team" : "pearl",
+    "title" : "test"
+    })
+    p_queue(msg)
+    return 'OK
+
+
 if __name__ == '__main__':
    app.run()
