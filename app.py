c1eec11e4847f6b435f0f942e0bb87b6118d486d
Jonathan Chan <jonoccchan@gmail.com>
2022-06-09 14:48:44 +0100

    send to correct queue functionality

--- app.py
+++ app.py
@@ -10,12 +10,39 @@ app.config["DEBUG"] = True
 def index():
     CONNECTION_STR = "Endpoint=sb://jcbugsystem.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=RnKgBHavWXnydS3wUygMQaBT/b7/bXDTWFEjtd5P2zY="
     PRIO_QUEUE_NAME = "priorityqueue"
+    STANDARD_QUEUE_NAME= "standardqueue"
     msg= json.dumps(request.json)
-    with ServiceBusClient.from_connection_string(CONNECTION_STR) as client:
-        with client.get_queue_sender(queue_name=PRIO_QUEUE_NAME) as sender:
-            message = ServiceBusMessage(msg)
-            sender.send_messages(message)
-    return render_template('index.html')
+    postMessage = json.loads(msg)
+    
+    if "severity" in postMessage:
+        if postMessage["severity"] == "black":
+            
+            with ServiceBusClient.from_connection_string(CONNECTION_STR) as client:
+                with client.get_queue_sender(queue_name=PRIO_QUEUE_NAME) as sender:
+                    message = ServiceBusMessage(msg)
+                    sender.send_messages(message)
+            return 'Severe bug report'
+        elif postMessage["severity"] == "red":
+            with ServiceBusClient.from_connection_string(CONNECTION_STR) as client:
+                with client.get_queue_sender(queue_name=STANDARD_QUEUE_NAME) as sender:
+                    message = ServiceBusMessage(msg)
+                    sender.send_messages(message)
+            return 'Standard bug report'
+        elif postMessage["severity"] == "blue":
+            with ServiceBusClient.from_connection_string(CONNECTION_STR) as client:
+                with client.get_queue_sender(queue_name=STANDARD_QUEUE_NAME) as sender:
+                    message = ServiceBusMessage(msg)
+                    sender.send_messages(message)
+            return 'Standard bug report'
+        else:
+            with open("log.txt", 'a') as json_file:
+                json.dump(request.json, json_file, indent=4, separators=(',',': '))
+            return 'Does not match required severity'
+    else:
+        with open("log.txt", 'a') as json_file:
+            json.dump(request.json, json_file, indent=4, separators=(',',': '))
+        return  'Missing severity' 
+    return 'OK'
 
 if __name__ == '__main__':
    app.run()
