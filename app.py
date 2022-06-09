0ab347f41589d46965f92ab24e488c633aa90eae
Jonathan Chan <jonoccchan@gmail.com>
2022-06-09 08:27:48 +0100

    removed variables

--- app.py
+++ app.py
@@ -4,21 +4,13 @@ import json
 from flask import Flask, request, jsonify, render_template, redirect, url_for, send_from_directory
 from azure.servicebus import ServiceBusClient, ServiceBusMessage
 
-
-
 app = Flask(__name__)
 app.config["DEBUG"] = True
-
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
-
-
-
-
-
 
 
 if __name__ == '__main__':
